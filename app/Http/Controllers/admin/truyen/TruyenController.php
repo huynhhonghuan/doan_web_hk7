7de23fefecbf649c22086a7f0d9c5385d9af4220
huynhhonghuan <huynhhonghuan@gmail.com>
2023-12-24 16:27:11 +0700

    2 code

---
+++ app/Http/Controllers/admin/truyen/TruyenController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\admin\truyen;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TruyenController extends Controller
+{
+    //
+}
