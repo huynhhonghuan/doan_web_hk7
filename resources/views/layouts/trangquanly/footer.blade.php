3af7e1581140a042d6ba89b72f6cd066139638ac
Dương Khải Duy <114801362+mokeyduy@users.noreply.github.com>
2023-12-27 12:30:36 +0700

    Add files via upload

---
+++ resources/views/layouts/trangquanly/footer.blade.php
@@ -0,0 +1,46 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; {{ date('Y') }} by ALo</strong>
+</footer>
+<script src="{{ asset('public/admin/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('public/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('public/admin/dist/js/adminlte.min.js') }}"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+{{-- <script>
+    $(document).ready(function() {
+        new DataTable('#tablephim', {
+            language: {
+                info: 'Hiện thị trang _PAGE_ trên _PAGES_',
+                infoEmpty: 'Không có phim nào',
+                infoFiltered: '(được lọc từ tổng số _MAX_ phim)',
+                lengthMenu: 'Hiện thị _MENU_ phim',
+                zeroRecords: 'Không tìm thấy',
+                search: 'Tìm kiếm: ',
+                paginate: {
+                    previous: '<<',
+                    next: '>>'
+                }
+            },
+            // scrollX: true,
+        });
+    });
+</script> --}}
+{{-- <script>
+    $('.select-year').change(function() {
+        var year = $(this).find(':selected').val();
+        var id_movie = $(this).attr('id');
+
+        //alert(year);
+        $.ajax({
+            url: "{{ route('admin.movie.update_year') }}",
+            method: "GET",
+            data: {
+                year: year,
+                id_movie: id_movie,
+            },
+            success: function() {
+                alert('Thay đổi năm phim thành năm ' + year + ' thành công');
+            }
+        });
+    })
+</script> --}}
+@yield('footer')
