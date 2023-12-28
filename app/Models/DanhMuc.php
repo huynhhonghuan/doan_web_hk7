f3d914b990001b3abb9273595bac8d7de034c4ed
Dương Khải Duy <114801362+mokeyduy@users.noreply.github.com>
2023-12-28 10:26:22 +0700

    Add files via upload

---
+++ app/Models/DanhMuc.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DanhMuc extends Model
+{
+    use HasFactory;
+    protected $table='danhmuc';
+
+    public function Phim()
+    {
+        return $this->hasMany(Phim::class, 'danhmuc_id')->orderBy('id','DESC')->where('khoa', 1);
+    }
+
+
+}
