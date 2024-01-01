ee1803eb4a54384bf5bb91909056d0a18f7f892f
Dương Khải Duy <114801362+mokeyduy@users.noreply.github.com>
2024-01-01 22:16:09 +0700

    Add files via upload

---
+++ resources/views/layouts/trangchu/head.blade.php
@@ -0,0 +1,77 @@
+<meta charset="utf-8" />
+<meta content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+<meta name="theme-color" content="#234556">
+<meta http-equiv="Content-Language" content="vi" />
+<meta content="VN" name="geo.region" />
+<meta name="DC.language" scheme="utf-8" content="vi" />
+<meta name="language" content="Việt Nam">
+
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="shortcut icon"
+    href="https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png"
+    type="image/x-icon" />
+<meta name="revisit-after" content="1 days" />
+<meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+<title>Phim hay {{ date('Y') }}- Xem phim hay nhất</title>
+<meta name="description" content="Phim hay 2021 - Xem phim hay nhất, xem phim online miễn phí, phim hot , phim nhanh" />
+<link rel="canonical" href="">
+<link rel="next" href="" />
+<meta property="og:locale" content="vi_VN" />
+<meta property="og:title" content="Phim hay 2020 - Xem phim hay nhất" />
+<meta property="og:description"
+    content="Phim hay 2020 - Xem phim hay nhất, phim hay trung quốc, hàn quốc, việt nam, mỹ, hong kong , chiếu rạp" />
+<meta property="og:url" content="" />
+<meta property="og:site_name" content="Phim hay 2021- Xem phim hay nhất" />
+<meta property="og:image" content="" />
+<meta property="og:image:width" content="300" />
+<meta property="og:image:height" content="55" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<link rel='dns-prefetch' href='//s.w.org' />
+
+<link rel='stylesheet' id='bootstrap-css' href="{{ asset('public/css/bootstrap.min.css') }}" media='all' />
+<link rel='stylesheet' id='style-css' href='{{ asset('public/css/style.css') }}' media='all' />
+<link rel='stylesheet' id='wp-block-library-css' href='{{ asset('public/css/style.min.css') }}' media='all' />
+{{-- <link rel="stylesheet" href="{{ asset('public/admin/plugins/fontawesome-free/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('public/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('public/admin/dist/css/adminlte.min.css') }}"> --}}
+<script type='text/javascript' src="{{ asset('public/js/jquery.min.js') }}" id='halim-jquery-js'></script>
+
+<style type="text/css" id="wp-custom-css">
+    .textwidget p a img {
+        width: 100%;
+    }
+</style>
+<style>
+    html,
+    body {
+        cursor: url("{{ asset('public/image/curmiku6.png') }}"), auto;
+    }
+
+    a:hover {
+        cursor: url("{{ asset('public/image/201661.png') }}"), auto;
+    }
+</style>
+<style>
+    #header .site-title {
+        background: url(https://www.pngkey.com/png/detail/360-3601772_your-logo-here-your-company-logo-here-png.png) no-repeat top left;
+        background-size: contain;
+        text-indent: -9999px;
+    }
+</style>
+
+<style>
+    ul#result {
+        position: absolute;
+        z-index: 9999;
+        background: #1b2d3c;
+        width: 94%;
+        padding: 10px;
+        margin: 1px;
+    }
+</style>
+<style>
+    .hl-down-open:before {
+        content: '▼';
+    }
+</style>
